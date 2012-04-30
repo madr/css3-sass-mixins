26e54d93d325c9508622d35d6d4cf92a7665300d
Anders Ytterström <anders.ytterstrom@gmail.com>
2012-04-30 11:01:33 +0200

    improved usability

--- site.js
+++ site.js
@@ -1,5 +1,5 @@
 /*jslint indent: 2, browser: true */
-(function(document){
+(function (document) {
   "use strict";
   var ins, i,
     inc, inc2,
@@ -9,6 +9,7 @@
     imagin,
     example,
     update,
+    callback,
     toggleIE;
 
   inc = "    %s\n";
@@ -19,30 +20,38 @@
   imagin = document.getElementById("imagin");
   example = document.getElementById("example");
 
+  callback = function () {
+    if (this.type === "checkbox") {
+      update(this.id,
+        this.nextSibling.innerHTML,
+        !this.checked);
+    } else {
+      toggleIE(this.value);
+    }
+  };
+
   while (i) {
     i -= 1;
-    ins[i].onchange = function () {
-      if (this.type === "checkbox") {
-        update(this.id,
-          this.nextSibling.innerHTML,
-          !this.checked);
-      } else {
-        toggleIE(this.value);
-      }
-    };
+    ins[i].onchange = callback;
   }
 
   toggleIE = function (decision) {
     rw = ie.replace("%d", decision);
-    imagin.value = imagin.value.replace(/useIEFilters.+?;/, rw);
-  }
+    imagin.value = imagin.value.replace(/useIEFilters: \d;/, rw);
+    if (parseInt(decision, 10) === 1) {
+      e = example.className += " legacy";
+    } else {
+      e = example.className.replace(" legacy", "");
+    }
+    example.className = e;
+  };
 
   update = function (id, value, checked) {
     t = inc.replace("%s", value);
     if (checked) {
       e = example.className.replace(" " + id, "");
       v = imagin.value.replace(t, "");
-      v = v.replace(inc2.replace("%s", value), "")
+      v = v.replace(inc2.replace("%s", value), "");
     } else {
       e = example.className + " " + id;
       v = imagin.value.replace("}", t + "}");
